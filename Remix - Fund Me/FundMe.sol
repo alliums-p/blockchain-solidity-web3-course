8f8c56732c0305bcc19120704e7f1295f58d0870
alliums-p <angnimas42@gmail.com>
2022-06-24 21:49:57 +0545

    withdraw function u;pdates

--- Remix - Fund Me/FundMe.sol
+++ Remix - Fund Me/FundMe.sol
@@ -9,8 +9,19 @@ contract FundMe {
     uint256 public minUsd = 50 * 1e18;
 
     address[] public funders;
-
     mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    // Used in case any conditional statements are to be used by multiple functions 
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Must be the owner!");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     // `Payable` function makes it so that ETH can be sent through the function
     function fund() public payable {
@@ -20,10 +31,21 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public payable {
-        require(address(this).balance > 0, "No balance");
-        address payable to = payable(msg.sender);
-        to.transfer(address(this).balance);
+    function withdraw() public onlyOwner {
+
+        /** starting index, ending index, step amount */
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reseting the array
+        funders = new address[](0);
+
+        // `transfer` automatically reverts if the transfer fails
+        // `send` only reverts transaction if a require statement is added
+        // `call` a low level function with 2 data confirmation and returns (recommended to use for transfer!!!)
+        (bool sent, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(sent, "Transfer failed!");
     }
 
 }
