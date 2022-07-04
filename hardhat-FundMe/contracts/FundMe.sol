a4dd5c3d3ba747fcddd60da8407b38b257c9b2bf
alliums <angnimas42@gmail.com>
2022-07-04 13:35:58 +0545

    hardhat fundme lesson

---
+++ hardhat-FundMe/contracts/FundMe.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+
+error FundMe__NotOwner();
+error FundMe__NotEnoughETH();
+
+/**
+    @title  A crowdfunding contract
+    @author Alliums
+    @notice Tutorial contract from FreeCodeCamp - Patrick Collins
+    @dev    This implements price feeds as our library
+*/
+contract FundMe {
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) private addressToAmountFunded;
+    address[] private funders;
+
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address private immutable i_owner;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+
+    AggregatorV3Interface private immutable priceFeed;
+
+    modifier onlyOwner {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+    
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    /**
+        @notice This function funds the contract
+        @dev    This implements price feeds as our library
+     */
+    function fund() public payable {
+        if(msg.value.getConversionRate(priceFeed) <= MINIMUM_USD) revert FundMe__NotEnoughETH();
+
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+    
+    function getVersion() public view returns (uint256){
+        return priceFeed.version();
+    }
+    
+    function withdraw() public payable onlyOwner {
+        address[] memory m_funders = funders;
+
+        for (
+            uint256 funderIndex = 0; 
+            funderIndex < m_funders.length; 
+            funderIndex++
+        ){
+            address funder = m_funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        
+        (bool callSuccess, ) = i_owner.call{value: address(this).balance}("");
+        require(callSuccess);
+    }
+
+    function getOwner() public view returns (address) {
+        return i_owner;
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return funders[index];
+    }
+
+    function getAddressToAmountFunded(address funder) public view returns (uint256) {
+        return addressToAmountFunded[funder];
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return priceFeed;
+    }
+
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \ 
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback() 
+    //     /   \ 
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+}
+
+// Concepts we didn't cover yet (will cover in later sections)
+// 1. Enum
+// 2. Events
+// 3. Try / Catch
+// 4. Function Selector
+// 5. abi.encode / decode
+// 6. Hash with keccak256
+// 7. Yul / Assembly
+
+
---
+++ hardhat-FundMe/hardhat.config.js
@@ -0,0 +1,80 @@
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// require("./tasks/block-number")
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const RINKEBY_RPC_URL = process.env.ALCHEMY_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API = process.env.ETHERSCAN_API || ""
+const COINMARKETCAP_API = process.env.COINMARKETCAP_API || ""
+
+module.exports = {
+    // solidity: "0.8.8",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8"
+            },
+            {
+                version: "0.6.6"
+            }
+        ]
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            timeout: 400000,
+            blockConfirmations: 6
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+            timeout: 400000,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            rinkeby: ETHERSCAN_API
+        },
+        customChains: [
+            {
+                network: "rinkeby",
+                chainId: 4,
+                urls: {
+                    apiURL: "https://api-rinkeby.etherscan.io/api",
+                    browserURL: "https://rinkeby.etherscan.io",
+                }
+            }
+        ]
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API,
+        // token: "MATIC", // specify different token for different blockchain
+    },
+    namedAccounts: {
+      deployer: {
+        default: 0,
+        
+      }
+    }
+}
