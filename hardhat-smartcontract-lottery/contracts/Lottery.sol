91c3ace1b5d3090be7110c0303ac69866837aa29
alliums-p <angnimas42@gmail.com>
2022-07-07 18:21:41 +0545

    Creating Lottery Contract

--- hardhat-smartcontract-lottery/contracts/Lottery.sol
+++ hardhat-smartcontract-lottery/contracts/Lottery.sol
@@ -2,23 +2,50 @@
 
 pragma solidity ^0.8.7;
 
-// import "@chainlink"
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Lottery__NotEnoughETH();
+error Lottery__TransferFailed();
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
+
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+    uint32 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+
 
     uint256 private s_lotteryFee;
     address payable[] private s_players;
 
+    address private s_recentWinner;
+
     // Events
     event LotteryParticipation(address indexed player);
+    event RequestedLotteryWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
-    constructor(uint256 lotteryFee) {
+    constructor(
+        address vrfCoordinatorV2, 
+        uint256 lotteryFee, 
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         s_lotteryFee = lotteryFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function enterLottery() {
+    function enterLottery() public payable {
         if(msg.value < s_lotteryFee) revert Lottery__NotEnoughETH();
         s_players.push(
             payable(msg.sender)
@@ -28,18 +55,40 @@ contract Lottery {
     }
 
     function requestRandomWinner() external {
-
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedLotteryWinner(requestId);
     }
 
-    function fulfillRandomWords() external {
+    function fulfillRandomWords(
+        uint256 /*requestId*/, 
+        uint256[] memory randomWords
+    ) 
+        internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
 
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if(!success) revert Lottery__TransferFailed();
+
+        emit WinnerPicked(recentWinner);
     }
 
     function getLotteryFee() public view returns (uint256) {
         return s_lotteryFee;
     }
 
-    function getPlater(uint256 index) public view returns (address) {
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
