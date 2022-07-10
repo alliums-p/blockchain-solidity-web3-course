dae1d966685928cf41adaefc43f3dd4c258d9d54
alliums-p <angnimas42@gmail.com>
2022-07-10 12:43:18 +0545

    writing deploy scripts and vrf mocks

--- hardhat-smartcontract-lottery/hardhat.config.js
+++ hardhat-smartcontract-lottery/hardhat.config.js
@@ -6,9 +6,36 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API = process.env.COINMARKETCAP_API;
+const ETHERSCAN_API = process.env.ETHERSCAN_API;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1
+    },
+    rinkeby: {
+      chainId: 4,
+      blockConfirmations: 6,
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  },
   solidity: "0.8.7",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    }
+  }
 };
