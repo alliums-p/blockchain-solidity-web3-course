744312d03d566e7ac381dee30c889ddd47fe7a28
alliums-p <angnimas42@gmail.com>
2022-07-10 18:13:09 +0545

    completion for deploy scripts, verification script of contract and configs

--- hardhat-smartcontract-lottery/deploy/01-deploy-lottery.js
+++ hardhat-smartcontract-lottery/deploy/01-deploy-lottery.js
@@ -1,13 +1,53 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+const {verify} = require("../utils/verify")
+
+const VRF_SUB_FUND_AMT = ethers.utils.parseEther("30");
 
 module.exports = async function({getNamedAccounts, deployments}) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+    let vrfCoordinatorV2Address, subscriptionId;
+
+    if(developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
+
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+
+        subscriptionId = transactionReceipt.events[0].args.subId
+
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMT)
+    }else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+    }
+
+    const entranceFee = networkConfig[chainId]["entranceFee"];
+    const gasLane = networkConfig[chainId]["gasLane"];
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"];
+    const interval = networkConfig[chainId]['interval']
+
+    const args = [
+        vrfCoordinatorV2Address, entranceFee, gasLane,
+        subscriptionId, callbackGasLimit, interval
+    ]
 
     const lottery = await deploy("Lottery", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API) {
+        log("Verifying...")
+        await verify(lottery.address, args)
+    }
+
+    log("===================================")
 }
+
+module.exports.tags = ["all", "lottery"]
