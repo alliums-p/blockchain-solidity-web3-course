490e97b4daa23b4778ac99127bad3aa709795746
alliums-p <angnimas42@gmail.com>
2022-08-23 13:49:47 +0545

    completed unit test for nft marketplace

--- full-stack-marketplace/hardhat-nft-marketplace/test/unit/NFTMarketplace.test.js
+++ full-stack-marketplace/hardhat-nft-marketplace/test/unit/NFTMarketplace.test.js
@@ -12,7 +12,7 @@ const { developmentChains } = require("../../helper.hardhat.config")
 
           beforeEach(async () => {
               accounts = await ethers.getSigners()
-              deployer = accounts.deployer
+              deployer = accounts[0]
               await deployments.fixture(["all"])
 
               marketplace = await ethers.getContract("NFTMarketplace", deployer)
@@ -63,6 +63,42 @@ const { developmentChains } = require("../../helper.hardhat.config")
               })
           })
 
+          describe("buyItem", function () {
+              it("reverts on low price buy", async () => {
+                  await marketplace.listItem(nft.address, tokenId, listPrice)
+                  marketplace = await marketplace.connect(accounts[1])
+
+                  await expect(
+                      marketplace.buyItem(nft.address, tokenId)
+                  ).to.be.revertedWith("NFTMarketplace__PriceNotMet")
+              })
+
+              it("reverts if item is not listed", async () => {
+                  await expect(
+                      marketplace.buyItem(nft.address, tokenId)
+                  ).to.be.revertedWith("NFTMarketplace__NFTNotListed")
+              })
+
+              it("transfers the nft to the buyer, updates the proceeds, and emits event", async () => {
+                  await marketplace.listItem(nft.address, tokenId, listPrice)
+                  marketplace = await marketplace.connect(accounts[1])
+
+                  expect(
+                      await marketplace.buyItem(nft.address, tokenId, {
+                          value: listPrice,
+                      })
+                  ).to.emit("ItemBought")
+
+                  const newOwner = await nft.ownerOf(tokenId)
+                  assert(newOwner.toString() == accounts[1].address)
+
+                  const proceeds = await marketplace.getProceeds(
+                      accounts[0].address
+                  )
+                  assert(proceeds.toString() == listPrice.toString())
+              })
+          })
+
           describe("cancelListing", function () {
               it("reverts if no listing is found", async function () {
                   const error = `NFTMarketplace__NFTNotListed("${nft.address}", ${tokenId})`
@@ -93,6 +129,77 @@ const { developmentChains } = require("../../helper.hardhat.config")
                       tokenId
                   )
                   assert(exists.price.toString() == "0")
+              })
+          })
+
+          describe("updateListing", function () {
+              it("must be listed and be owner", async () => {
+                  const error = `NFTMarketplace__NFTNotListed("${nft.address}", ${tokenId})`
+                  await expect(
+                      marketplace.updateListing(nft.address, tokenId, listPrice)
+                  ).to.be.revertedWith(error)
+
+                  await marketplace.listItem(nft.address, tokenId, listPrice)
+
+                  marketplace = await marketplace.connect(accounts[1])
+                  await expect(
+                      marketplace.updateListing(nft.address, tokenId, listPrice)
+                  ).to.be.revertedWith("NFTMarketplace__NotNFTOwner()")
+              })
+
+              it("emits event and updates price", async () => {
+                  await marketplace.listItem(nft.address, tokenId, listPrice)
+
+                  const newPrice = ethers.utils.parseEther("0.1")
+                  expect(
+                      await marketplace.updateListing(
+                          nft.address,
+                          tokenId,
+                          newPrice
+                      )
+                  ).to.emit("ItemListed")
+
+                  const listing = await marketplace.getListing(
+                      nft.address,
+                      tokenId
+                  )
+                  assert(listing.price.toString() == newPrice)
+              })
+          })
+
+          describe("withdrawProceeds", function () {
+              it("thorws error on 0 proceed withdrawls", async () => {
+                  await expect(
+                      marketplace.withdrawProceeds()
+                  ).to.be.revertedWith("NFTMarketplace__NoProceeds()")
+              })
+
+              it("withdraws proceeds succesfully", async () => {
+                  await marketplace.listItem(nft.address, tokenId, listPrice)
+                  marketplace = marketplace.connect(accounts[1])
+
+                  await marketplace.buyItem(nft.address, tokenId, {
+                      value: listPrice,
+                  })
+
+                  marketplace = marketplace.connect(accounts[0])
+
+                  const proceedBeforeWithdraw = await marketplace.getProceeds(
+                      accounts[0].address
+                  )
+                  const balanceBefore = await deployer.getBalance()
+
+                  const withdrawTx = await marketplace.withdrawProceeds()
+                  const txReceipt = await withdrawTx.wait(1)
+                  const { gasUsed, effectiveGasPrice } = txReceipt
+                  const gasCost = gasUsed.mul(effectiveGasPrice)
+
+                  const balanceAfter = await deployer.getBalance()
+
+                  assert(
+                      balanceAfter.add(gasCost).toString() ==
+                          proceedBeforeWithdraw.add(balanceBefore).toString()
+                  )
               })
           })
       })
